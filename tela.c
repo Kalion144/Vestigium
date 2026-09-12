1bc63b6b132442e3ebc55d552b7051cb40f06ae9
Kalion144 <162513924+Kalion144@users.noreply.github.com>
2026-09-12 14:43:17 -0300

    correçao de tela

--- tela.c
+++ tela.c
@@ -1,28 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 #include "tela.h"
 
-void limparTela() {     // Função para limpar a tela
-        system("cls");
-    }
+void limparTela() {
+    system("cls");
+}
 
-    void esperarEnter () {      // Função para esperar o usuário pressionar Enter
-        printf("\n              Pressione Enter para continuar...");
-        while (getchar() != '\n');
-    }
+void esperarEnter() {
 
-    void mostrarTexto(const char *linha1, const char *linha2, const char *linha3) { // Função para mostrar o texto na tela
+    printf("\n              Pressione Enter para continuar...");
 
-        limparTela();
+    int tecla;
 
-        printf("\n");
-        printf("====================================================\n");
-        printf("|                                                  \n");
-        printf("|  %s \n", linha1);
-        printf("|  %s \n", linha2);
-        printf("|  %s \n", linha3);
-        printf("|                                                  \n");
-        printf("====================================================\n");
+    do {
+        tecla = _getch();
+    } while (tecla != 13);
+}
 
-        esperarEnter();
-    }
+void mostrarTexto(
+    const char *linha1,
+    const char *linha2,
+    const char *linha3
+) {
+
+    limparTela();
+
+    printf("\n");
+    printf("====================================================\n");
+    printf("|                                                  \n");
+    printf("|  %s\n", linha1);
+    printf("|  %s\n", linha2);
+    printf("|  %s\n", linha3);
+    printf("|                                                  \n");
+    printf("====================================================\n");
+
+    esperarEnter();
+}
